08d0b64fc68e5ae610bc658d8395c9a404c53913
Marton <hello@martonborzak.com>
2020-01-05 12:44:12 +0100

    New translations en_US.ts (Spanish)

--- integration.home-assistant/translated/es_ES.ts
+++ integration.home-assistant/translated/es_ES.ts
@@ -6,12 +6,12 @@
     <message>
       <location filename="../homeassistant.cpp" line="257"/>
       <source>Cannot connect to Home Assistant.</source>
-      <translation type="unfinished">Cannot connect to Home Assistant.</translation>
+      <translation>No se pudo conectar a Home Assistant.</translation>
     </message>
     <message>
       <location filename="../homeassistant.cpp" line="257"/>
       <source>Reconnect</source>
-      <translation type="unfinished">Reconnect</translation>
+      <translation>Volver a conectar</translation>
     </message>
   </context>
 </TS>
