5b4a2be19298470068a57b834b250ab0ce861418
Marton <hello@martonborzak.com>
2020-03-20 21:15:54 +0100

    New translations en_US.ts (Italian)

--- integration.home-assistant/translated/it_IT.ts
+++ integration.home-assistant/translated/it_IT.ts
@@ -4,18 +4,18 @@
   <context>
     <name>HomeAssistant</name>
     <message>
-      <location filename="../src/homeassistant.cpp" line="194"/>
+      <location filename="../src/homeassistant.cpp" line="196"/>
       <source>Cannot connect to Home Assistant.</source>
       <translation>Impossibile connettersi a Home Assistant.</translation>
     </message>
     <message>
-      <location filename="../src/homeassistant.cpp" line="194"/>
-      <location filename="../src/homeassistant.cpp" line="532"/>
+      <location filename="../src/homeassistant.cpp" line="196"/>
+      <location filename="../src/homeassistant.cpp" line="535"/>
       <source>Reconnect</source>
       <translation>Riconnetti</translation>
     </message>
     <message>
-      <location filename="../src/homeassistant.cpp" line="532"/>
+      <location filename="../src/homeassistant.cpp" line="535"/>
       <source>Connection lost to </source>
       <translation>Connessione persa verso </translation>
     </message>
