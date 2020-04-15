df25950dfc7e4c4747a22cb445a509078138ff94
Marton <hello@martonborzak.com>
2020-04-15 18:18:30 +0200

    Create en_US.ts

---
+++ web-configurator/en_US.ts
@@ -0,0 +1,13 @@
+import { ILocale } from '../types';
+
+export default {
+	settingsPage: {
+		darkMode: 'Dark Mode',
+		autoBrightness: 'Auto Brightness',
+	},
+	disconnectionOverlay: {
+		title: 'Oh No!',
+		message: 'It looks like we lost connection with your remote. Please make sure it is accessible on the network.',
+		reconnecting: 'Reconnecting',
+	}
+} as ILocale;
