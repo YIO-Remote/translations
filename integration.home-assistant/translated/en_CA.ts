c77269bec99a2edcd1d2464c28b875de6a6812e1
Marton <hello@martonborzak.com>
2020-09-10 09:38:38 +0200

    New translations en_US.ts (English, Canada)

---
+++ integration.home-assistant/translated/en_CA.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en-CA" sourcelanguage="en">
+  <context>
+    <name>HomeAssistant</name>
+    <message>
+      <location filename="../src/homeassistant.cpp" line="237"/>
+      <source>Cannot connect to </source>
+      <translation type="unfinished">Cannot connect to </translation>
+    </message>
+    <message>
+      <location filename="../src/homeassistant.cpp" line="237"/>
+      <location filename="../src/homeassistant.cpp" line="632"/>
+      <source>Reconnect</source>
+      <translation type="unfinished">Reconnect</translation>
+    </message>
+    <message>
+      <location filename="../src/homeassistant.cpp" line="267"/>
+      <source>SSL certificate validation error. Please check your certificate. </source>
+      <translation type="unfinished">SSL certificate validation error. Please check your certificate. </translation>
+    </message>
+    <message>
+      <location filename="../src/homeassistant.cpp" line="632"/>
+      <source>Connection lost to </source>
+      <translation type="unfinished">Connection lost to </translation>
+    </message>
+  </context>
+</TS>
