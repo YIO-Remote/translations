cd278ad7a9fc8ba0ad40d3009b0f28bb2a347afb
Marton <hello@martonborzak.com>
2020-09-15 16:18:11 +0200

    New translations en_US.ts (Swedish)

--- remote-software/translated/sv_SE.ts
+++ remote-software/translated/sv_SE.ts
@@ -723,7 +723,7 @@ för att konfigurera YIO-fjärrkontrollen</translation>
       <location filename="../sources/main.cpp" line="210"/>
       <source>Factory reset failed.
  %1</source>
-      <translation type="unfinished">Factory reset failed.
+      <translation>Fabriksåterställning misslyckades.
  %1</translation>
     </message>
   </context>
