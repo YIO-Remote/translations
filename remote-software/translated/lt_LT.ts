376ebc17784eb7ed3dddea4464b95d9e9af50fd2
Marton <hello@martonborzak.com>
2020-10-15 21:57:03 +0200

    New translations en_US.ts (Lithuanian)

--- remote-software/translated/lt_LT.ts
+++ remote-software/translated/lt_LT.ts
@@ -128,7 +128,7 @@ Jei norite sužinoti daugiau apie projektą, eikite
   <context>
     <name>BluetoothControl</name>
     <message>
-      <location filename="../sources/bluetooth.cpp" line="41"/>
+      <location filename="../sources/bluetooth.cpp" line="39"/>
       <source>Bluetooth device was not found.</source>
       <translation>Bluetooth įrenginys nerastas.</translation>
     </message>
@@ -387,32 +387,32 @@ Jei norite sužinoti daugiau apie projektą, eikite
   <context>
     <name>CommandLineHandler</name>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="45"/>
+      <location filename="../sources/commandlinehandler.cpp" line="42"/>
       <source>Use configuration profile.</source>
       <translation>Naudokite nustatymų profilį.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="48"/>
+      <location filename="../sources/commandlinehandler.cpp" line="45"/>
       <source>Use configuration file.</source>
       <translation>Naudokite nustatymų failą.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="49"/>
+      <location filename="../sources/commandlinehandler.cpp" line="46"/>
       <source>Use configuration schema file.</source>
       <translation>Naudokite nustatymų failą.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="50"/>
+      <location filename="../sources/commandlinehandler.cpp" line="47"/>
       <source>Use hardware configuration file.</source>
       <translation>Naudokite įrangos nustatymų failą.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="51"/>
+      <location filename="../sources/commandlinehandler.cpp" line="48"/>
       <source>Use hardware configuration schema file.</source>
       <translation>Naudokite įrangos nustatymų failą.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="55"/>
+      <location filename="../sources/commandlinehandler.cpp" line="52"/>
       <source>Validate json configuration files and exit.</source>
       <translation>Patvirtinkite json nustatymų failus ir išeikite.</translation>
     </message>
@@ -541,12 +541,12 @@ kad sutvarkyti šią problemą.</translation>
   <context>
     <name>FileDownload</name>
     <message>
-      <location filename="../sources/filedownload.cpp" line="114"/>
+      <location filename="../sources/filedownload.cpp" line="112"/>
       <source>Not enough free space (%1 MB).</source>
       <translation>Trūksta laisvos vietos (%1 MB).</translation>
     </message>
     <message>
-      <location filename="../sources/filedownload.cpp" line="121"/>
+      <location filename="../sources/filedownload.cpp" line="119"/>
       <source>Destination file already exists</source>
       <translation>Toks failas jau egzistuoja</translation>
     </message>
@@ -597,30 +597,38 @@ YIO distancinio valdymo pulto nustatymams</translation>
   <context>
     <name>JsonFile</name>
     <message>
-      <location filename="../sources/jsonfile.cpp" line="80"/>
-      <location filename="../sources/jsonfile.cpp" line="133"/>
+      <location filename="../sources/jsonfile.cpp" line="78"/>
+      <location filename="../sources/jsonfile.cpp" line="131"/>
       <source>empty name</source>
       <translation>pavadinimas tuščias</translation>
     </message>
     <message>
-      <location filename="../sources/jsonfile.cpp" line="86"/>
+      <location filename="../sources/jsonfile.cpp" line="84"/>
       <source>empty data</source>
       <translation>nėra duomenų</translation>
     </message>
     <message>
-      <location filename="../sources/jsonfile.cpp" line="97"/>
+      <location filename="../sources/jsonfile.cpp" line="95"/>
       <source>cannot open file &apos;%1&apos; for writing: %2</source>
       <translation>negali atidaryti failo &apos;%1&apos; įrašymui: %2</translation>
     </message>
     <message>
-      <location filename="../sources/jsonfile.cpp" line="140"/>
+      <location filename="../sources/jsonfile.cpp" line="138"/>
       <source>cannot open file &apos;%1&apos; for reading: %2</source>
       <translation>negali atidaryti failo &apos;%1&apos; skaitymui: %2</translation>
     </message>
     <message>
-      <location filename="../sources/jsonfile.cpp" line="149"/>
+      <location filename="../sources/jsonfile.cpp" line="147"/>
       <source>invalid JSON file &apos;%1&apos; at offset %2</source>
       <translation>blogas JSON turinys &apos;%1&apos; šioje vietoje %2</translation>
+    </message>
+  </context>
+  <context>
+    <name>Logger</name>
+    <message>
+      <location filename="../sources/logger.cpp" line="300"/>
+      <source>Error creating log file &apos;%1&apos;.</source>
+      <translation type="unfinished">Error creating log file &apos;%1&apos;.</translation>
     </message>
   </context>
   <context>
@@ -720,7 +728,7 @@ YIO distancinio valdymo pulto nustatymams</translation>
   <context>
     <name>QGuiApplication</name>
     <message>
-      <location filename="../sources/main.cpp" line="210"/>
+      <location filename="../sources/main.cpp" line="216"/>
       <source>Factory reset failed.
  %1</source>
       <translation>Nepavyko atstatyti gamyklinius nustatymus.
@@ -730,7 +738,7 @@ YIO distancinio valdymo pulto nustatymams</translation>
   <context>
     <name>QObject</name>
     <message>
-      <location filename="../sources/hardware/wifi_control.h" line="37"/>
+      <location filename="../sources/hardware/wifi_control.h" line="36"/>
       <source>WiFi device was not found.</source>
       <translation>Bevielio tinklo įrenginys nerastas.</translation>
     </message>
@@ -1067,52 +1075,52 @@ po distancinio pulto perkrovimo eikite į
   <context>
     <name>SoftwareUpdate</name>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="165"/>
+      <location filename="../sources/softwareupdate.cpp" line="163"/>
       <source>Cannot connect to the update server.</source>
       <translation>Nepavyko prisijungti prie atnaujinimų serverio.</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="196"/>
+      <location filename="../sources/softwareupdate.cpp" line="194"/>
       <source>Invalid request.</source>
       <translation>Bloga užklausa.</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="199"/>
+      <location filename="../sources/softwareupdate.cpp" line="197"/>
       <source>Unsupported device.</source>
       <translation>Nepalaikomas įrenginys.</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="202"/>
+      <location filename="../sources/softwareupdate.cpp" line="200"/>
       <source>Service currently not available.</source>
       <translation>Paslauga šiuo metu negalima.</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="205"/>
+      <location filename="../sources/softwareupdate.cpp" line="203"/>
       <source>Request error %1</source>
       <translation>Užklausos klaida %1</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="209"/>
+      <location filename="../sources/softwareupdate.cpp" line="207"/>
       <source>Software update:</source>
       <translation>Programinės įrangos atnaujinimas:</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="226"/>
+      <location filename="../sources/softwareupdate.cpp" line="224"/>
       <source>New software is available</source>
       <translation>Rasta nauja programinės įrangos versija</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="226"/>
+      <location filename="../sources/softwareupdate.cpp" line="224"/>
       <source>Download</source>
       <translation>Atsisiųsti</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="243"/>
+      <location filename="../sources/softwareupdate.cpp" line="241"/>
       <source>The remote needs to be at least 50% battery to perform updates.</source>
       <translation>Distancinio valdymo pultas turi būti įkrautas mažiausiai 50% jei norite atnaujinti programinę įrangą.</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="303"/>
+      <location filename="../sources/softwareupdate.cpp" line="301"/>
       <source>Download failed: %1</source>
       <translation>Atsisiųsti nepavyko: %1</translation>
     </message>
