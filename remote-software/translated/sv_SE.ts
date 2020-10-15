186dcb8e4d87cbc9d8c4b0832783517da6b43a30
Marton <hello@martonborzak.com>
2020-10-15 21:56:34 +0200

    New translations en_US.ts (Swedish)

--- remote-software/translated/sv_SE.ts
+++ remote-software/translated/sv_SE.ts
@@ -128,7 +128,7 @@ För att lära dig mer om projektet, besök
   <context>
     <name>BluetoothControl</name>
     <message>
-      <location filename="../sources/bluetooth.cpp" line="41"/>
+      <location filename="../sources/bluetooth.cpp" line="39"/>
       <source>Bluetooth device was not found.</source>
       <translation>Bluetooth-enheten kunde inte hittas.</translation>
     </message>
@@ -387,32 +387,32 @@ För att lära dig mer om projektet, besök
   <context>
     <name>CommandLineHandler</name>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="45"/>
+      <location filename="../sources/commandlinehandler.cpp" line="42"/>
       <source>Use configuration profile.</source>
       <translation>Använd konfigurationsprofil.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="48"/>
+      <location filename="../sources/commandlinehandler.cpp" line="45"/>
       <source>Use configuration file.</source>
       <translation>Använd konfigurationsfil.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="49"/>
+      <location filename="../sources/commandlinehandler.cpp" line="46"/>
       <source>Use configuration schema file.</source>
       <translation>Använd konfigurationsschemafil.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="50"/>
+      <location filename="../sources/commandlinehandler.cpp" line="47"/>
       <source>Use hardware configuration file.</source>
       <translation>Använd hårdvarukonfigurationsfil.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="51"/>
+      <location filename="../sources/commandlinehandler.cpp" line="48"/>
       <source>Use hardware configuration schema file.</source>
       <translation>Använd hårdvarukonfigurationsschemafil.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="55"/>
+      <location filename="../sources/commandlinehandler.cpp" line="52"/>
       <source>Validate json configuration files and exit.</source>
       <translation>Validera json konfigurationsfiler och avsluta.</translation>
     </message>
@@ -541,12 +541,12 @@ för att åtgärda problemet.</translation>
   <context>
     <name>FileDownload</name>
     <message>
-      <location filename="../sources/filedownload.cpp" line="114"/>
+      <location filename="../sources/filedownload.cpp" line="112"/>
       <source>Not enough free space (%1 MB).</source>
       <translation>Inte tillräckligt med ledigt utrymme (%1 MB).</translation>
     </message>
     <message>
-      <location filename="../sources/filedownload.cpp" line="121"/>
+      <location filename="../sources/filedownload.cpp" line="119"/>
       <source>Destination file already exists</source>
       <translation>Målfilen finns redan</translation>
     </message>
@@ -597,30 +597,38 @@ för att konfigurera YIO-fjärrkontrollen</translation>
   <context>
     <name>JsonFile</name>
     <message>
-      <location filename="../sources/jsonfile.cpp" line="80"/>
-      <location filename="../sources/jsonfile.cpp" line="133"/>
+      <location filename="../sources/jsonfile.cpp" line="78"/>
+      <location filename="../sources/jsonfile.cpp" line="131"/>
       <source>empty name</source>
       <translation>tomt namn</translation>
     </message>
     <message>
-      <location filename="../sources/jsonfile.cpp" line="86"/>
+      <location filename="../sources/jsonfile.cpp" line="84"/>
       <source>empty data</source>
       <translation>tom data</translation>
     </message>
     <message>
-      <location filename="../sources/jsonfile.cpp" line="97"/>
+      <location filename="../sources/jsonfile.cpp" line="95"/>
       <source>cannot open file &apos;%1&apos; for writing: %2</source>
       <translation>kan inte öppna fil &apos;%1&apos; för skrivning: %2</translation>
     </message>
     <message>
-      <location filename="../sources/jsonfile.cpp" line="140"/>
+      <location filename="../sources/jsonfile.cpp" line="138"/>
       <source>cannot open file &apos;%1&apos; for reading: %2</source>
       <translation>kan inte öppna fil &apos;%1&apos; för läsning: %2</translation>
     </message>
     <message>
-      <location filename="../sources/jsonfile.cpp" line="149"/>
+      <location filename="../sources/jsonfile.cpp" line="147"/>
       <source>invalid JSON file &apos;%1&apos; at offset %2</source>
       <translation>ogiltig JSON-fil &apos;%1&apos; vid offset %2</translation>
+    </message>
+  </context>
+  <context>
+    <name>Logger</name>
+    <message>
+      <location filename="../sources/logger.cpp" line="300"/>
+      <source>Error creating log file &apos;%1&apos;.</source>
+      <translation type="unfinished">Error creating log file &apos;%1&apos;.</translation>
     </message>
   </context>
   <context>
@@ -720,7 +728,7 @@ för att konfigurera YIO-fjärrkontrollen</translation>
   <context>
     <name>QGuiApplication</name>
     <message>
-      <location filename="../sources/main.cpp" line="210"/>
+      <location filename="../sources/main.cpp" line="216"/>
       <source>Factory reset failed.
  %1</source>
       <translation>Fabriksåterställning misslyckades.
@@ -730,7 +738,7 @@ för att konfigurera YIO-fjärrkontrollen</translation>
   <context>
     <name>QObject</name>
     <message>
-      <location filename="../sources/hardware/wifi_control.h" line="37"/>
+      <location filename="../sources/hardware/wifi_control.h" line="36"/>
       <source>WiFi device was not found.</source>
       <translation>WiFi-enheten kunde inte hittas.</translation>
     </message>
@@ -1067,52 +1075,52 @@ Fjärrkonfiguration efter omstart av fjärrkontrollen.</translation>
   <context>
     <name>SoftwareUpdate</name>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="165"/>
+      <location filename="../sources/softwareupdate.cpp" line="163"/>
       <source>Cannot connect to the update server.</source>
       <translation>Kan inte ansluta till uppdateringsservern.</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="196"/>
+      <location filename="../sources/softwareupdate.cpp" line="194"/>
       <source>Invalid request.</source>
       <translation>Förfrågan var ogiltig.</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="199"/>
+      <location filename="../sources/softwareupdate.cpp" line="197"/>
       <source>Unsupported device.</source>
       <translation>Enheten stöds ej.</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="202"/>
+      <location filename="../sources/softwareupdate.cpp" line="200"/>
       <source>Service currently not available.</source>
       <translation>Tjänsten är för närvarande inte tillgänglig.</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="205"/>
+      <location filename="../sources/softwareupdate.cpp" line="203"/>
       <source>Request error %1</source>
       <translation>Fel vid begäran %1</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="209"/>
+      <location filename="../sources/softwareupdate.cpp" line="207"/>
       <source>Software update:</source>
       <translation>Programvaruuppdatering:</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="226"/>
+      <location filename="../sources/softwareupdate.cpp" line="224"/>
       <source>New software is available</source>
       <translation>Ny version är tillgänglig</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="226"/>
+      <location filename="../sources/softwareupdate.cpp" line="224"/>
       <source>Download</source>
       <translation>Ladda ned</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="243"/>
+      <location filename="../sources/softwareupdate.cpp" line="241"/>
       <source>The remote needs to be at least 50% battery to perform updates.</source>
       <translation>Fjärrkontrollen måste ha minst 50% batteri för att utföra uppdateringar.</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="303"/>
+      <location filename="../sources/softwareupdate.cpp" line="301"/>
       <source>Download failed: %1</source>
       <translation>Nedladdning misslyckades: %1</translation>
     </message>
