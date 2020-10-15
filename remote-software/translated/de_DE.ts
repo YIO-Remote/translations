5e1108cd32491a4f82f551677234dbdf102c70e6
Marton <hello@martonborzak.com>
2020-10-15 21:56:01 +0200

    New translations en_US.ts (German)

--- remote-software/translated/de_DE.ts
+++ remote-software/translated/de_DE.ts
@@ -128,7 +128,7 @@ Um mehr über das Projekt zu erfahren, besuche
   <context>
     <name>BluetoothControl</name>
     <message>
-      <location filename="../sources/bluetooth.cpp" line="41"/>
+      <location filename="../sources/bluetooth.cpp" line="39"/>
       <source>Bluetooth device was not found.</source>
       <translation>Bluetooth Gerät wurde nicht gefunden.</translation>
     </message>
@@ -387,32 +387,32 @@ Um mehr über das Projekt zu erfahren, besuche
   <context>
     <name>CommandLineHandler</name>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="45"/>
+      <location filename="../sources/commandlinehandler.cpp" line="42"/>
       <source>Use configuration profile.</source>
       <translation>Konfigurationsprofil verwenden.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="48"/>
+      <location filename="../sources/commandlinehandler.cpp" line="45"/>
       <source>Use configuration file.</source>
       <translation>Konfigurationsdatei verwenden.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="49"/>
+      <location filename="../sources/commandlinehandler.cpp" line="46"/>
       <source>Use configuration schema file.</source>
       <translation>Konfigurationsschema-Datei verwenden.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="50"/>
+      <location filename="../sources/commandlinehandler.cpp" line="47"/>
       <source>Use hardware configuration file.</source>
       <translation>Hardware-Konfigurationsdatei verwenden.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="51"/>
+      <location filename="../sources/commandlinehandler.cpp" line="48"/>
       <source>Use hardware configuration schema file.</source>
       <translation>Hardware-Konfigurationsschema-Datei verwenden.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="55"/>
+      <location filename="../sources/commandlinehandler.cpp" line="52"/>
       <source>Validate json configuration files and exit.</source>
       <translation>Überprüfen Sie json Konfigurationsdateien und beenden Sie diese.</translation>
     </message>
@@ -541,12 +541,12 @@ um das Problem zu lösen.</translation>
   <context>
     <name>FileDownload</name>
     <message>
-      <location filename="../sources/filedownload.cpp" line="114"/>
+      <location filename="../sources/filedownload.cpp" line="112"/>
       <source>Not enough free space (%1 MB).</source>
       <translation>Nicht genug freier Speicherplatz (%1 MB).</translation>
     </message>
     <message>
-      <location filename="../sources/filedownload.cpp" line="121"/>
+      <location filename="../sources/filedownload.cpp" line="119"/>
       <source>Destination file already exists</source>
       <translation>Zieldatei existiert bereits</translation>
     </message>
@@ -596,30 +596,38 @@ to set up YIO remote</source>
   <context>
     <name>JsonFile</name>
     <message>
-      <location filename="../sources/jsonfile.cpp" line="80"/>
-      <location filename="../sources/jsonfile.cpp" line="133"/>
+      <location filename="../sources/jsonfile.cpp" line="78"/>
+      <location filename="../sources/jsonfile.cpp" line="131"/>
       <source>empty name</source>
       <translation>leerer Name</translation>
     </message>
     <message>
-      <location filename="../sources/jsonfile.cpp" line="86"/>
+      <location filename="../sources/jsonfile.cpp" line="84"/>
       <source>empty data</source>
       <translation>Leere Daten</translation>
     </message>
     <message>
-      <location filename="../sources/jsonfile.cpp" line="97"/>
+      <location filename="../sources/jsonfile.cpp" line="95"/>
       <source>cannot open file &apos;%1&apos; for writing: %2</source>
       <translation>Datei &apos;%1&apos; kann nicht geschrieben werden: %2</translation>
     </message>
     <message>
-      <location filename="../sources/jsonfile.cpp" line="140"/>
+      <location filename="../sources/jsonfile.cpp" line="138"/>
       <source>cannot open file &apos;%1&apos; for reading: %2</source>
       <translation>Datei &apos;%1&apos; kann nicht gelesen werden: %2</translation>
     </message>
     <message>
-      <location filename="../sources/jsonfile.cpp" line="149"/>
+      <location filename="../sources/jsonfile.cpp" line="147"/>
       <source>invalid JSON file &apos;%1&apos; at offset %2</source>
       <translation>ungültige JSON-Datei &apos;%1&apos; bei Offset %2</translation>
+    </message>
+  </context>
+  <context>
+    <name>Logger</name>
+    <message>
+      <location filename="../sources/logger.cpp" line="300"/>
+      <source>Error creating log file &apos;%1&apos;.</source>
+      <translation type="unfinished">Error creating log file &apos;%1&apos;.</translation>
     </message>
   </context>
   <context>
@@ -719,7 +727,7 @@ to set up YIO remote</source>
   <context>
     <name>QGuiApplication</name>
     <message>
-      <location filename="../sources/main.cpp" line="210"/>
+      <location filename="../sources/main.cpp" line="216"/>
       <source>Factory reset failed.
  %1</source>
       <translation>Zurücksetzen auf Werkseinstellungen fehlgeschlagen
@@ -729,7 +737,7 @@ to set up YIO remote</source>
   <context>
     <name>QObject</name>
     <message>
-      <location filename="../sources/hardware/wifi_control.h" line="37"/>
+      <location filename="../sources/hardware/wifi_control.h" line="36"/>
       <source>WiFi device was not found.</source>
       <translation>WiFi-Gerät nicht gefunden.</translation>
     </message>
@@ -1066,52 +1074,52 @@ den Konfigurator nach einem Neustart an.</translation>
   <context>
     <name>SoftwareUpdate</name>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="165"/>
+      <location filename="../sources/softwareupdate.cpp" line="163"/>
       <source>Cannot connect to the update server.</source>
       <translation>Keine Verbindung zum Update-Server.</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="196"/>
+      <location filename="../sources/softwareupdate.cpp" line="194"/>
       <source>Invalid request.</source>
       <translation>Ungültige Anfrage.</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="199"/>
+      <location filename="../sources/softwareupdate.cpp" line="197"/>
       <source>Unsupported device.</source>
       <translation>Nicht unterstütztes Gerät.</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="202"/>
+      <location filename="../sources/softwareupdate.cpp" line="200"/>
       <source>Service currently not available.</source>
       <translation>Dienst ist derzeit nicht verfügbar.</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="205"/>
+      <location filename="../sources/softwareupdate.cpp" line="203"/>
       <source>Request error %1</source>
       <translation>Anfrage-Fehler %1</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="209"/>
+      <location filename="../sources/softwareupdate.cpp" line="207"/>
       <source>Software update:</source>
       <translation>Software-Update:</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="226"/>
+      <location filename="../sources/softwareupdate.cpp" line="224"/>
       <source>New software is available</source>
       <translation>Neue Software ist verfügbar</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="226"/>
+      <location filename="../sources/softwareupdate.cpp" line="224"/>
       <source>Download</source>
       <translation>Herunterladen</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="243"/>
+      <location filename="../sources/softwareupdate.cpp" line="241"/>
       <source>The remote needs to be at least 50% battery to perform updates.</source>
       <translation>Die YIO Remote muss mindestens zu 50% geladen sein, um Updates auszuführen.</translation>
     </message>
     <message>
-      <location filename="../sources/softwareupdate.cpp" line="303"/>
+      <location filename="../sources/softwareupdate.cpp" line="301"/>
       <source>Download failed: %1</source>
       <translation>Download fehlgeschlagen: %1</translation>
     </message>
