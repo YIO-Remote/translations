7c711179346ddd5d111c6c4f9996b07efdbb3c20
Marton <hello@martonborzak.com>
2020-10-16 15:49:36 +0200

    New translations en_US.ts (Dutch)

--- remote-software/translated/nl_NL.ts
+++ remote-software/translated/nl_NL.ts
@@ -728,7 +728,7 @@ om de YIO afstandsbediening in te stellen</translation>
   <context>
     <name>QGuiApplication</name>
     <message>
-      <location filename="../sources/main.cpp" line="216"/>
+      <location filename="../sources/main.cpp" line="217"/>
       <source>Factory reset failed.
  %1</source>
       <translation type="unfinished">Factory reset failed.
