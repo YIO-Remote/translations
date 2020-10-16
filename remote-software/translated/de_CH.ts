77891f9748d0630fd3bd95dc9aa33580056a3a5d
Marton <hello@martonborzak.com>
2020-10-16 15:48:57 +0200

    New translations en_US.ts (German, Switzerland)

--- remote-software/translated/de_CH.ts
+++ remote-software/translated/de_CH.ts
@@ -725,7 +725,7 @@ dini YIO Fernbedienig izrichtä</translation>
   <context>
     <name>QGuiApplication</name>
     <message>
-      <location filename="../sources/main.cpp" line="216"/>
+      <location filename="../sources/main.cpp" line="217"/>
       <source>Factory reset failed.
  %1</source>
       <translation type="unfinished">Factory reset failed.
