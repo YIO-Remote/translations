3535989df671f668ad4e2d0456afa8815bcc8a23
Marton <hello@martonborzak.com>
2020-10-16 15:48:42 +0200

    New translations en_US.ts (Chinese Traditional)

--- remote-software/translated/zh_TW.ts
+++ remote-software/translated/zh_TW.ts
@@ -726,7 +726,7 @@ to set up YIO remote</source>
   <context>
     <name>QGuiApplication</name>
     <message>
-      <location filename="../sources/main.cpp" line="216"/>
+      <location filename="../sources/main.cpp" line="217"/>
       <source>Factory reset failed.
  %1</source>
       <translation>恢復出廠設置失敗。
