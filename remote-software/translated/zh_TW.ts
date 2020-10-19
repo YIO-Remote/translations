dd13fa7ac0cd224ff1186e6531b4993279135876
Marton <hello@martonborzak.com>
2020-10-19 09:17:36 +0200

    New translations en_US.ts (Chinese Traditional)

--- remote-software/translated/zh_TW.ts
+++ remote-software/translated/zh_TW.ts
@@ -126,7 +126,7 @@ To learn more about the project, visit
   <context>
     <name>BluetoothControl</name>
     <message>
-      <location filename="../sources/bluetooth.cpp" line="39"/>
+      <location filename="../sources/bluetooth.cpp" line="40"/>
       <source>Bluetooth device was not found.</source>
       <translation>找不到藍芽裝置。</translation>
     </message>
@@ -726,7 +726,7 @@ to set up YIO remote</source>
   <context>
     <name>QGuiApplication</name>
     <message>
-      <location filename="../sources/main.cpp" line="217"/>
+      <location filename="../sources/main.cpp" line="221"/>
       <source>Factory reset failed.
  %1</source>
       <translation>恢復出廠設置失敗。
@@ -985,7 +985,7 @@ a power source and wait until it starts blinking.
   <context>
     <name>SetupStep8</name>
     <message>
-      <location filename="../setup/SetupStep8.qml" line="136"/>
+      <location filename="../setup/SetupStep8.qml" line="143"/>
       <source>Setting up your YIO Dock</source>
       <translation>設定你的 YIO DOCK</translation>
     </message>
