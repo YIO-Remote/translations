f4510b2b6c39c84a2dd9422b6ab907825eb369b9
Marton <hello@martonborzak.com>
2020-10-19 09:18:26 +0200

    New translations en_US.ts (Hungarian)

--- remote-software/translated/hu_HU.ts
+++ remote-software/translated/hu_HU.ts
@@ -128,7 +128,7 @@ Tudj meg többet a projektről:
   <context>
     <name>BluetoothControl</name>
     <message>
-      <location filename="../sources/bluetooth.cpp" line="39"/>
+      <location filename="../sources/bluetooth.cpp" line="40"/>
       <source>Bluetooth device was not found.</source>
       <translation type="unfinished">Bluetooth device was not found.</translation>
     </message>
@@ -728,7 +728,7 @@ to set up YIO remote</translation>
   <context>
     <name>QGuiApplication</name>
     <message>
-      <location filename="../sources/main.cpp" line="217"/>
+      <location filename="../sources/main.cpp" line="221"/>
       <source>Factory reset failed.
  %1</source>
       <translation type="unfinished">Factory reset failed.
@@ -989,7 +989,7 @@ A WiFi beállítás megosztásához érintsd meg a következőt.</translation>
   <context>
     <name>SetupStep8</name>
     <message>
-      <location filename="../setup/SetupStep8.qml" line="136"/>
+      <location filename="../setup/SetupStep8.qml" line="143"/>
       <source>Setting up your YIO Dock</source>
       <translation>A YIO dokkolód beállítása</translation>
     </message>
