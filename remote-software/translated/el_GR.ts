68876c8750d86caa47695e2631933ecc977d3e40
Marton <hello@martonborzak.com>
2020-10-19 09:18:21 +0200

    New translations en_US.ts (Greek)

--- remote-software/translated/el_GR.ts
+++ remote-software/translated/el_GR.ts
@@ -128,7 +128,7 @@ To learn more about the project, visit
   <context>
     <name>BluetoothControl</name>
     <message>
-      <location filename="../sources/bluetooth.cpp" line="39"/>
+      <location filename="../sources/bluetooth.cpp" line="40"/>
       <source>Bluetooth device was not found.</source>
       <translation>Δεν βρέθηκαν συσκευές bluetooth.</translation>
     </message>
@@ -728,7 +728,7 @@ to set up YIO remote</source>
   <context>
     <name>QGuiApplication</name>
     <message>
-      <location filename="../sources/main.cpp" line="217"/>
+      <location filename="../sources/main.cpp" line="221"/>
       <source>Factory reset failed.
  %1</source>
       <translation type="unfinished">Factory reset failed.
@@ -987,7 +987,7 @@ a power source and wait until it starts blinking.
   <context>
     <name>SetupStep8</name>
     <message>
-      <location filename="../setup/SetupStep8.qml" line="136"/>
+      <location filename="../setup/SetupStep8.qml" line="143"/>
       <source>Setting up your YIO Dock</source>
       <translation>Ρύθμιση της βάσης YIO</translation>
     </message>
