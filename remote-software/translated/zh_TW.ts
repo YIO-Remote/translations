9d38300b8c451be1521aafc6c94ef184ff9bd161
Marton <hello@martonborzak.com>
2020-11-15 19:20:28 +0100

    New translations en_US.ts (Chinese Traditional)

--- remote-software/translated/zh_TW.ts
+++ remote-software/translated/zh_TW.ts
@@ -126,7 +126,7 @@ To learn more about the project, visit
   <context>
     <name>BluetoothControl</name>
     <message>
-      <location filename="../sources/bluetooth.cpp" line="40"/>
+      <location filename="../sources/bluetooth.cpp" line="46"/>
       <source>Bluetooth device was not found.</source>
       <translation>找不到藍芽裝置。</translation>
     </message>
@@ -410,7 +410,12 @@ To learn more about the project, visit
       <translation>使用硬件設定模式檔</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="52"/>
+      <location filename="../sources/commandlinehandler.cpp" line="50"/>
+      <source>Show frames / second counter.</source>
+      <translation type="unfinished">Show frames / second counter.</translation>
+    </message>
+    <message>
+      <location filename="../sources/commandlinehandler.cpp" line="53"/>
       <source>Validate json configuration files and exit.</source>
       <translation>驗證 json 配置文件並退出</translation>
     </message>
@@ -726,7 +731,7 @@ to set up YIO remote</source>
   <context>
     <name>QGuiApplication</name>
     <message>
-      <location filename="../sources/main.cpp" line="221"/>
+      <location filename="../sources/main.cpp" line="220"/>
       <source>Factory reset failed.
  %1</source>
       <translation>恢復出廠設置失敗。
@@ -985,7 +990,7 @@ a power source and wait until it starts blinking.
   <context>
     <name>SetupStep8</name>
     <message>
-      <location filename="../setup/SetupStep8.qml" line="143"/>
+      <location filename="../setup/SetupStep8.qml" line="144"/>
       <source>Setting up your YIO Dock</source>
       <translation>設定你的 YIO DOCK</translation>
     </message>
