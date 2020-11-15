5058d977ec4edfcf804455a6609f71ea94a94b65
Marton <hello@martonborzak.com>
2020-11-15 19:21:29 +0100

    New translations en_US.ts (Portuguese)

--- remote-software/translated/pt_PT.ts
+++ remote-software/translated/pt_PT.ts
@@ -128,7 +128,7 @@ Para saber mais sobre o projeto, visite
   <context>
     <name>BluetoothControl</name>
     <message>
-      <location filename="../sources/bluetooth.cpp" line="40"/>
+      <location filename="../sources/bluetooth.cpp" line="46"/>
       <source>Bluetooth device was not found.</source>
       <translation>O dispositivo Bluetooth não foi encontrado.</translation>
     </message>
@@ -412,7 +412,12 @@ Para saber mais sobre o projeto, visite
       <translation type="unfinished">Use hardware configuration schema file.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="52"/>
+      <location filename="../sources/commandlinehandler.cpp" line="50"/>
+      <source>Show frames / second counter.</source>
+      <translation type="unfinished">Show frames / second counter.</translation>
+    </message>
+    <message>
+      <location filename="../sources/commandlinehandler.cpp" line="53"/>
       <source>Validate json configuration files and exit.</source>
       <translation type="unfinished">Validate json configuration files and exit.</translation>
     </message>
@@ -728,7 +733,7 @@ to set up YIO remote</translation>
   <context>
     <name>QGuiApplication</name>
     <message>
-      <location filename="../sources/main.cpp" line="221"/>
+      <location filename="../sources/main.cpp" line="220"/>
       <source>Factory reset failed.
  %1</source>
       <translation>Falha ao repor os valores iniciais
@@ -989,7 +994,7 @@ a power source and wait until it starts blinking.
   <context>
     <name>SetupStep8</name>
     <message>
-      <location filename="../setup/SetupStep8.qml" line="143"/>
+      <location filename="../setup/SetupStep8.qml" line="144"/>
       <source>Setting up your YIO Dock</source>
       <translation type="unfinished">Setting up your YIO Dock</translation>
     </message>
