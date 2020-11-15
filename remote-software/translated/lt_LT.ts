30df87d306d036c2565559437d9f04ccb7c5ab10
Marton <hello@martonborzak.com>
2020-11-15 19:21:22 +0100

    New translations en_US.ts (Lithuanian)

--- remote-software/translated/lt_LT.ts
+++ remote-software/translated/lt_LT.ts
@@ -128,7 +128,7 @@ Jei norite sužinoti daugiau apie projektą, eikite
   <context>
     <name>BluetoothControl</name>
     <message>
-      <location filename="../sources/bluetooth.cpp" line="40"/>
+      <location filename="../sources/bluetooth.cpp" line="46"/>
       <source>Bluetooth device was not found.</source>
       <translation>Bluetooth įrenginys nerastas.</translation>
     </message>
@@ -412,7 +412,12 @@ Jei norite sužinoti daugiau apie projektą, eikite
       <translation>Naudokite įrangos nustatymų failą.</translation>
     </message>
     <message>
-      <location filename="../sources/commandlinehandler.cpp" line="52"/>
+      <location filename="../sources/commandlinehandler.cpp" line="50"/>
+      <source>Show frames / second counter.</source>
+      <translation type="unfinished">Show frames / second counter.</translation>
+    </message>
+    <message>
+      <location filename="../sources/commandlinehandler.cpp" line="53"/>
       <source>Validate json configuration files and exit.</source>
       <translation>Patvirtinkite json nustatymų failus ir išeikite.</translation>
     </message>
@@ -728,7 +733,7 @@ YIO distancinio valdymo pulto nustatymams</translation>
   <context>
     <name>QGuiApplication</name>
     <message>
-      <location filename="../sources/main.cpp" line="221"/>
+      <location filename="../sources/main.cpp" line="220"/>
       <source>Factory reset failed.
  %1</source>
       <translation>Nepavyko atstatyti gamyklinius nustatymus.
@@ -989,7 +994,7 @@ elektros tinklo ir lukterkite kol lemputė pradės mirksėti.
   <context>
     <name>SetupStep8</name>
     <message>
-      <location filename="../setup/SetupStep8.qml" line="143"/>
+      <location filename="../setup/SetupStep8.qml" line="144"/>
       <source>Setting up your YIO Dock</source>
       <translation>YIO stovelio nustatymai</translation>
     </message>
