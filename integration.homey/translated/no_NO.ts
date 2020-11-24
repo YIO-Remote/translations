e9be1ce194ce02924c829aa879187e1b1ffa2315
Marton <hello@martonborzak.com>
2020-11-24 12:02:48 +0100

    New translations en_US.ts (Norwegian)

--- integration.homey/translated/no_NO.ts
+++ integration.homey/translated/no_NO.ts
@@ -6,12 +6,12 @@
     <message>
       <location filename="../src/homey.cpp" line="201"/>
       <source>Cannot connect to Homey.</source>
-      <translation type="unfinished">Cannot connect to Homey.</translation>
+      <translation>Kan ikke koble til Homey.</translation>
     </message>
     <message>
       <location filename="../src/homey.cpp" line="201"/>
       <source>Reconnect</source>
-      <translation type="unfinished">Reconnect</translation>
+      <translation>Koble til på nytt</translation>
     </message>
   </context>
 </TS>
