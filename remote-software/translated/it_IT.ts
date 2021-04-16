b19fb4de9a438bebc35ab8b93dd6550145a8c825
Marton <hello@martonborzak.com>
2021-04-16 18:48:13 +0200

    New translations en_US.ts (Italian)

--- remote-software/translated/it_IT.ts
+++ remote-software/translated/it_IT.ts
@@ -615,17 +615,22 @@ per impostare YIO Remote</translation>
   <context>
     <name>Launcher</name>
     <message>
-      <location filename="../sources/launcher.cpp" line="52"/>
+      <location filename="../sources/launcher.cpp" line="55"/>
       <source>Incompatible plugin %1: metadata missing</source>
       <translation>Plugin %1 incompatibile: metadati mancanti</translation>
     </message>
     <message>
-      <location filename="../sources/launcher.cpp" line="57"/>
+      <location filename="../sources/launcher.cpp" line="60"/>
       <source>Incompatible plugin %1: integration lib mismatch</source>
       <translation>Plugin %1 incompatibile: lib d'integrazione errata</translation>
     </message>
     <message>
-      <location filename="../sources/launcher.cpp" line="64"/>
+      <location filename="../sources/launcher.cpp" line="66"/>
+      <source>Plugin %1 requires app version %2 or higher</source>
+      <translation type="unfinished">Plugin %1 requires app version %2 or higher</translation>
+    </message>
+    <message>
+      <location filename="../sources/launcher.cpp" line="73"/>
       <source>Failed to load %1</source>
       <translation>Caricamento %1 fallito</translation>
     </message>
